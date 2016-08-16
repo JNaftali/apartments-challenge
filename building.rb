0c53033c04d6a8bd32f291622fe1c0bed68f4de5
Joshua Marantz <email": null>
2016-08-15 20:55:55 -0400

    All tests pass

--- building.rb
+++ building.rb
@@ -1,2 +1,27 @@
 class Building
+  attr_reader :apartments
+
+  def initialize(args={})
+    @apartments = args.fetch(:apartments, [])
+  end
+
+  def total_room_count
+    apartments.reduce(0) {|total, apt| total + apt.room_count}
+  end
+
+  def total_monthly_revenue
+    apartments.reduce(0) {|total, apt| total + apt.monthly_rent}
+  end
+
+  def apartments_by_rent
+    apartments.sort
+  end
+
+  def find_apartments_by_bedroom_count(number_of_bedrooms)
+    apartments.find {|apt| apt.bedroom_count == number_of_bedrooms}
+  end
+
+  def total_sqft
+    apartments.reduce(0) {|total, apt| total + apt.total_sqft}
+  end
 end
